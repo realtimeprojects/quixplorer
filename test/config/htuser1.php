3c90898cf32c6bccb00e32c639cbe270f4e6669f
Claudio Klingler <ck@realtime-projects.com>
2011-03-04 17:22:00 +0100

    user.php-test: Added several test cases

--- test/config/htuser1.php
+++ test/config/htuser1.php
@@ -1,3 +1,4 @@
 <?php $GLOBALS["users"]=array(
 	array("admin","9628d0d187029e6337baa86780b2abb6",".","http://localhost",1,"",7,1),
+	array("guest","fc5e038d38a57032085441e7fe7010b0","./data","http://localhost",0,"",7,1),
 ); ?>
