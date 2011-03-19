230ddaa53698aa3b4f7d12859d6d99343671f033
Claudio Klingler <ck@realtime-projects.com>
2011-03-19 07:39:19 +0100

    Check: In fun_edit_save_file: stripslashes has been deactivated

--- src/_include/fun_edit.php
+++ src/_include/fun_edit.php
@@ -40,7 +40,8 @@ Comment:
 require_once("./_include/permissions.php");
 //------------------------------------------------------------------------------
 function savefile($file_name) {			// save edited file
-	$code = stripslashes($GLOBALS['__POST']["code"]);
+	//$code = stripslashes($GLOBALS['__POST']["code"]);
+	$code = $GLOBALS['__POST']["code"];
 	$fp = @fopen($file_name, "w");
 	if($fp===false) show_error(basename($file_name).": ".$GLOBALS["error_msg"]["savefile"]);
 	fputs($fp, $code);
