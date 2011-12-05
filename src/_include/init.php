67adfe67eac88acae04a97a11cdb8ffd41e3eded
Claudio Klingler <ck@realtime-projects.com>
2011-12-05 23:32:38 +0100

    Quixplorer prints out error message if no config file was found.

--- src/_include/init.php
+++ src/_include/init.php
@@ -1,44 +1,7 @@
 <?php
-/*------------------------------------------------------------------------------
-     The contents of this file are subject to the Mozilla Public License
-     Version 1.1 (the "License"); you may not use this file except in
-     compliance with the License. You may obtain a copy of the License at
-     http://www.mozilla.org/MPL/
 
-     Software distributed under the License is distributed on an "AS IS"
-     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-     License for the specific language governing rights and limitations
-     under the License.
+require "./_include/error.php";
 
-     The Original Code is init.php, released on 2003-03-31.
-
-     The Initial Developer of the Original Code is The QuiX project.
-
-     Alternatively, the contents of this file may be used under the terms
-     of the GNU General Public License Version 2 or later (the "GPL"), in
-     which case the provisions of the GPL are applicable instead of
-     those above. If you wish to allow use of your version of this file only
-     under the terms of the GPL and not to allow others to use
-     your version of this file under the MPL, indicate your decision by
-     deleting  the provisions above and replace  them with the notice and
-     other provisions required by the GPL.  If you do not delete
-     the provisions above, a recipient may use your version of this file
-     under either the MPL or the GPL."
-------------------------------------------------------------------------------*/
-/*------------------------------------------------------------------------------
-Author: The QuiX project
-	quix@free.fr
-	http://www.quix.tk
-	http://quixplorer.sourceforge.net
-
-Comment:
-	QuiXplorer Version 2.3
-	Main File
-	
-	Have Fun...
-------------------------------------------------------------------------------*/
-//------------------------------------------------------------------------------
-// Vars
 if(isset($_SERVER)) {
 	$GLOBALS['__GET']	=&$_GET;
 	$GLOBALS['__POST']	=&$_POST;
@@ -83,6 +46,10 @@ elseif(isset($GLOBALS['__POST']["lang"])) $GLOBALS["lang"]=$GLOBALS['__POST']["l
 //------------------------------------------------------------------------------
 // Necessary files
 ob_start(); // prevent unwanted output
+
+if (!is_readable("./_config/conf.php"))
+    show_error("./_config/conf.php not found.. please see installation instructions");
+
 require "./_config/conf.php";
 require "./_config/configs.php";
 if(isset($GLOBALS["lang"])) $GLOBALS["language"]=$GLOBALS["lang"];
@@ -90,9 +57,8 @@ require "./_lang/".$GLOBALS["language"].".php";
 require "./_lang/".$GLOBALS["language"]."_mimes.php";
 require "./_config/mimes.php";
 require "./_include/fun_extra.php";
-require "./_include/header.php";
+require_once "./_include/header.php";
 require "./_include/footer.php";
-require "./_include/error.php";
 ob_start(); // prevent unwanted output
 require_once "./_include/login.php";
 ob_end_clean(); // get rid of cached unwanted output
