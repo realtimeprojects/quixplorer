04c48017620cfe6e6f0dd4ff531b66ca39a5e989
Claudio Klingler <ck@realtime-projects.com>
2011-12-24 12:26:36 +0100

    +qx_title(), +qx_user()

--- src/qx.php
+++ src/qx.php
@@ -4,4 +4,16 @@ function qx_language()
     global $language;
     print $language;
 }
+
+function qx_title()
+{
+    global $title;
+    print $title;
+}
+
+function qx_user()
+{
+    //FIXME return real user
+    print "FIXME_USER";
+}
 ?>
