28c9effb9879190bb9a82f1b0d03fc96216ad624
Claudio Klingler <ck@realtime-projects.com>
2011-12-24 12:02:35 +0100

    Added templates header and footer

---
+++ src/_templates/default/footer.php
@@ -0,0 +1,9 @@
+<div class="footer">
+    <hr>
+    <small>
+        <a class="title" href="https://github.com/realtimeprojects/quixplorer" target="_blank"> QuiXplorer Version 2.5.1ALPHA </a>
+   </small>
+</div>
+</div> <!-- qx class -->
+</body>
+</html>
---
+++ src/_templates/default/header.php
@@ -0,0 +1,12 @@
+<html lang="<?php qx_language() ?>" >
+<head>
+    <meta http-equiv="Content-Type" content="text/html">
+    <title><?php qx_title() ?></title>
+    <link href=style.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+<div class="qx">
+    <div class="title">
+    <?php echo qx_user() . " - " . qx_title() ?>
+    </div>
