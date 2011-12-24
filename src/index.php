bd7861d1bb41260c4f4c39c8ef5413b87784b297
Claudio Klingler <ck@realtime-projects.com>
2011-12-24 12:04:03 +0100

    index.php: removed noise

--- src/index.php
+++ src/index.php
@@ -1,43 +1,5 @@
 <?php
-/*------------------------------------------------------------------------------
-     The contents of this file are subject to the Mozilla Public License
-     Version 1.1 (the "License"); you may not use this file except in
-     compliance with the License. You may obtain a copy of the License at
-     http://www.mozilla.org/MPL/
-
-     Software distributed under the License is distributed on an "AS IS"
-     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
-     License for the specific language governing rights and limitations
-     under the License.
-
-     The Original Code is index.php, released on 2003-04-02.
-
-     The Initial Developer of the Original Code is The QuiX project.
-
-     Alternatively, the contents of this file may be used under the terms
-     of the GNU General Public License Version 2 or later (the "GPL"), in
-     which case the provisions of the GPL are applicable instead of
-     those above. If you wish to allow use of your version of this file only
-     under the terms of the GPL and not to allow others to use
-     your version of this file under the MPL, indicate your decision by
-     deleting  the provisions above and replace  them with the notice and
-     other provisions required by the GPL.  If you do not delete
-     the provisions above, a recipient may use your version of this file
-     under either the MPL or the GPL."
-------------------------------------------------------------------------------*/
-/*------------------------------------------------------------------------------
-Authors: quix@free.fr, ck@realtime-projects.com
-	http://quixplorer.sourceforge.net
-
-Comment:
-	QuiXplorer Version 2.4.2
-	Main File
-	
-	Have Fun...
-------------------------------------------------------------------------------*/
-//------------------------------------------------------------------------------
 umask(002); // Added to make created files/dirs group writable
-//------------------------------------------------------------------------------
 require "./_include/init.php";	// Init
 //------------------------------------------------------------------------------
 switch($GLOBALS["action"]) {		// Execute action
