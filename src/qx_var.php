7d7260b96bf3b8ee2159020b2905595201dd5a76
Claudio Klingler <ck@realtime-projects.com>
2011-12-24 13:01:00 +0100

    +qx_var_authenticated()

--- src/qx_var.php
+++ src/qx_var.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+    @returns true, if a valid user has been authenticated
+*/
+function qx_var_authenticated ()
+{
+    //FIXME check real authentication
+    return false;
+}
+
 function qx_var_template_dir ()
 {
     //FIXME read out configuration
