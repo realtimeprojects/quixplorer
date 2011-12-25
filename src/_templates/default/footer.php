aa38f62b5cab33252b0216d04dd015337b271df8
Claudio Klingler <ck@realtime-projects.com>
2011-12-25 13:59:20 +0100

    footer: added link to login

--- src/_templates/default/footer.php
+++ src/_templates/default/footer.php
@@ -2,6 +2,9 @@
     <hr>
     <small>
         <a class="title" href="https://github.com/realtimeprojects/quixplorer" target="_blank"> QuiXplorer Version 2.5.1ALPHA </a>
+        <?php if (!qx_var_authenticated()) { ?>
+        <a href="<?php echo qx_link("login"); ?>">login</a>
+        <?php } ?>
    </small>
 </div>
 </div> <!-- qx class -->
