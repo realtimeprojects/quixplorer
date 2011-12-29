0dff39dce8c67326a54560579097b31544d455d6
Claudio Klingler <ck@realtime-projects.com>
2011-12-29 17:50:26 +0100

    qx_link() now knows download action

--- src/qx_link.php
+++ src/qx_link.php
@@ -1,9 +1,12 @@
 <?php
 function qx_link($what, $flags)
 {
-    if ($ret = preg_replace("/^(login|list|authenticate|chmod)$/", "?action=$1" . $flags, $what))
-        return $ret;
+    // check for valid commands
+    $ret = preg_replace("/^(login|list|authenticate|chmod|download)$/", "?action=$1" . $flags, $what);
 
-    return "?action=unknown";
+    // check if a replacement took place, if not, we do not know the link and return unknown
+    $ret = (($ret == NULL || $ret == $what)) ? "?action=unknown" : $ret;
+    _debug("qx_link(): made link to $ret");
+    return $ret;
 }
 ?> 
