24cd9d4be6a29c8822408a87fd4920ab06ce059a
Claudio Klingler <ck@realtime-projects.com>
2012-03-02 14:27:34 +0100

    Added debugging module.

---
+++ src/_include/debug.php
@@ -0,0 +1,35 @@
+<?php
+
+function testprint($what)
+{
+	echo "<h2>$what</h2>";
+}
+
+function _debug ($data)
+{
+	$debug = 1;
+
+	if ($debug == 0)
+		return;
+
+	$fp = fopen("debug.log", "a");
+	fputs($fp, date("Y-m-d H:i:s") . " quixplorer: $data\n");
+	fclose($fp);
+}
+
+// prints out an error message, but keeps your program running
+function _error ($data)
+{
+    // we also print out the error message to the debug log, if activated
+    _debug("ERROR: " . $data);
+
+	$activated = true; // place false here if you want to deactivate the error log
+
+	if ($activated == 0)
+		return;
+
+	$fp = fopen("error.log", "a");
+	fputs($fp, date("Y-m-d H:i:s") . " quixplorer: ERROR: $data\n");
+	fclose($fp);
+}
+?>
--- src/_include/error.php
+++ src/_include/error.php
@@ -2,11 +2,15 @@
 
 require_once "./_include/header.php";
 
+require_once "./_include/debug.php";
+
 /**
     show error-message and terminate
  */
 function show_error($error,$extra=NULL)
 {
+    _error( $error . " : " . $extra );
+
     // we do not know whether the language module was already loaded
     $errmsg = isset($GLOBALS["error_msg"]) ? $GLOBALS["error_msg"]["error"] : "ERROR";
     $backmsg = isset($GLOBALS["error_msg"]) ? $GLOBALS["error_msg"]["back"] : "BACK";
