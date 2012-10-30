994ee5182f562fb24e81f7b5502dde0e560a5218
Claudio Klingler <ck@realtime-projects.com>
2012-10-30 13:15:52 +0100

    debug also writes to syslog.
    Conflicts:
    	src/_include/debug.php

--- src/_include/debug.php
+++ src/_include/debug.php
@@ -1,32 +1,42 @@
 <?php
 
+function _syslog($level, $message)
+{
+    openlog("quixplorer", LOG_PID | LOG_PERROR | LOG_LOCAL0, LOG_USER);
+    syslog($level, $message);
+    closelog();
+}
+
 function testprint($what)
 {
-	echo "<h2>$what</h2>";
+    echo "<h2>$what</h2>";
 }
 
 function _debug ($data)
 {
-	$debug = 1;
+    global $FD_LOG;
 
-	if ($debug == 0)
-		return;
+    $debug = 1;
 
-	$fp = fopen("debug.log", "a");
-	fputs($fp, "quixplorer: " . date("Y-m-d H:i:s") . ": $data\n");
-	fclose($fp);
+    if ($debug == 0)
+      return;
+
+    _syslog(LOG_DEBUG, $data);
+    $fp = fopen("debug.log", "a");
+    fputs($fp, date("Y-m-d H:i:s") . " quixplorer: $data\n");
+    fclose($fp);
 }
 
 // prints out an error message, but keeps your program running
 function _error ($data)
 {
     // we also print out the error message to the debug log, if activated
-    _debug("ERROR: " . $data);
+    _syslog(LOG_ERR, $data);
 
-	$activated = true; // place false here if you want to deactivate the error log
+    $activated = true; // place false here if you want to deactivate the error log
 
-	if ($activated == 0)
-		return;
+    if ($activated == 0)
+        return;
 
 	$fp = fopen("error.log", "a");
 	fputs($fp, "quixplorer: " . date("Y-m-d H:i:s") . ": ERROR: $data\n");
