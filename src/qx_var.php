b79b361539f51c76140e68358608eb58fce85ca5
Claudio Klingler <ck@realtime-projects.com>
2013-02-14 13:26:18 +0100

    Refactored qx_var a bit.

--- src/qx_var.php
+++ src/qx_var.php
@@ -1,20 +1,30 @@
 <?php
-
-require_once("qx_cfg.php");
-require_once("_include/session.php");
-
 /**
-    @returns true, if a valid user has been authenticated
-*/
-function qx_var_authenticated ()
-{
-    $user = session_get("s_user");
-    return isset($user);
-}
+  Access to runtime variables.
 
-function qx_var_template_dir ()
+  This module offers access to runtime variables like the root directory
+  for qx or where to find the template directory.
+ */
+
+require_once "_include/log.php";
+require_once "qx_cfg.php";
+require_once "_include/session.php";
+
+function qx_var($variable)
 {
-    return qx_cfg("template_dir", "_templates/default");
+    switch ($variable)
+    {
+        // @returns true, if a valid user has been authenticated
+        case 'is_authenticated':
+            $active = session_get("s_user");
+            return $active;
+
+        // the path to the template directory
+        case 'template_dir':     return qx_cfg("template_dir", "_templates/default");
+    }
+
+    log_fatal("request for unknown session variable '$variable'");
+    return NULL;
 }
 
 ?>
