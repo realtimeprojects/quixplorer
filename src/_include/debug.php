fbc3289a5ccb366004c86b2a93ab34e242b1942d
Claudio Klingler <ck@realtime-projects.com>
2013-02-15 13:48:19 +0100

    Disabled file debug logging.

--- src/_include/debug.php
+++ src/_include/debug.php
@@ -21,10 +21,7 @@ function _debug ($data)
     if ($debug == 0)
       return;
 
-    _syslog(LOG_DEBUG, $data);
-    $fp = fopen("debug.log", "a");
-    fputs($fp, date("Y-m-d H:i:s") . " quixplorer: $data\n");
-    fclose($fp);
+    _syslog(LOG_NOTICE, $data);
 }
 
 // prints out an error message, but keeps your program running
@@ -32,14 +29,5 @@ function _error ($data)
 {
     // we also print out the error message to the debug log, if activated
     _syslog(LOG_ERR, $data);
-
-    $activated = true; // place false here if you want to deactivate the error log
-
-    if ($activated == 0)
-        return;
-
-    $fp = fopen("error.log", "a");
-    fputs($fp, date("Y-m-d H:i:s") . " quixplorer: ERROR: $data\n");
-    fclose($fp);
 }
 ?>
