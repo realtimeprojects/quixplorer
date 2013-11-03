6df4755f91a10321654e52be720b7abd94fe496f
Claudio Klingler <ck@realtime-projects.com>
2013-11-04 00:04:02 +0100

    +Test 005: listing

---
+++ test/features/system/005-list.feature
@@ -0,0 +1,25 @@
+Feature: List file contents
+    In order to explore the server file conent
+    i list the content of the server
+
+    Scenario: Perparation
+        Given I have the reference configuration
+
+    Scenario: List
+        When I list
+        Then I expect success
+
+    Scenario: List contains files
+        When I list
+        Then I find file xx in list
+        Then I find file huhu.txt in list
+        Then I find file haha.txt in list
+        Then I miss file nonexistent.txt in list
+
+    Scenario: List contains symbolic links
+        When I list
+        Then I find file yy in list
+
+    Scenario: List contains invalid symbolic links
+        When I list
+        Then I find file zy in list
--- test/radish/loginsteps.py
+++ test/radish/loginsteps.py
@@ -1,3 +1,5 @@
+import re
+
 from radish import step, world
 import quixplorer
 
@@ -5,4 +7,21 @@ import quixplorer
 def I_login(step, login_data):
     (world.result, world.output, world.stderr ) = quixplorer.run('login', [ login_data ])
     assert world.result == 0, "login failed: %d\n%s" % (exitcode, p.stderr.read())
+
+@step(u'I list(?: with \((.*)\))?')
+def I_list(step, list_data):
+    (world.result, world.output, world.stderr ) = quixplorer.run('list', [ list_data])
+    assert world.result == 0, "list failed: %d\n%s" % (exitcode, p.stderr.read())
+
+@step(u'I find file (.*) in list')
+def I_find_file_xx_in_list(step, filename):
+    assert _has_file(filename, world.output), "file %s not found in %s" % ( filename, world.output)
+
+@step(u'I miss (.*) in list')
+def I_miss_file_xx_in_list(step, filename):
+    assert _has_file(filename, world.output) == False, "file %s not found in %s" % ( filename, world.output)
+
+def _has_file(filename, output):
+    filter = 'value="%s"' % filename
+    return re.search(filter, output) != None
 
