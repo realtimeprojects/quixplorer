ebd7352de19a41f47246b16126254601f6a025ce
Claudio Klingler <ck@realtime-projects.com>
2013-11-04 14:13:51 +0100

    We only do syslog logging

--- src/_include/debug.php
+++ src/_include/debug.php
@@ -14,32 +14,12 @@ function testprint($what)
 
 function _debug ($data)
 {
-    global $FD_LOG;
-
-    $debug = 1;
-
-    if ($debug == 0)
-      return;
-
-    _syslog(LOG_DEBUG, $data);
-    $fp = fopen("debug.log", "a");
-    fputs($fp, date("Y-m-d H:i:s") . " quixplorer: $data\n");
-    fclose($fp);
+    _syslog(LOG_NOTICE, $data);
 }
 
 // prints out an error message, but keeps your program running
 function _error ($data)
 {
-    // we also print out the error message to the debug log, if activated
     _syslog(LOG_ERR, $data);
-
-    $activated = true; // place false here if you want to deactivate the error log
-
-    if ($activated == 0)
-        return;
-
-	$fp = fopen("error.log", "a");
-	fputs($fp, "quixplorer: " . date("Y-m-d H:i:s") . ": ERROR: $data\n");
-	fclose($fp);
 }
 ?>
