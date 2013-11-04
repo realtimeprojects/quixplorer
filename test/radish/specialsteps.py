b3480984f4e74ee7b67a1cbee0fef31f08ba916a
Claudio Klingler <ck@realtime-projects.com>
2013-11-04 14:08:30 +0100

    Testing: corrected result presentation

--- test/radish/specialsteps.py
+++ test/radish/specialsteps.py
@@ -9,14 +9,14 @@ from expressions import Expressions
 @step(r'I login to quixplorer%(args)s' % { "args":Expressions.args })
 def I_login(step, login_data):
     (world.result, world.output, world.stderr ) = quixplorer.run('login', [ login_data ])
-    assert world.result == 0, "login failed: %d\n%s" % (exitcode, p.stderr.read())
+    assert world.result == 0, "login failed: %d\n%s" % (world.result, world.stderr)
 
 # ** steps for list feature of quixplorer
 
 @step(u'I list%(args)s' % { "args":Expressions.args} )
 def I_list(step, list_data):
-    (world.result, world.output, world.stderr ) = quixplorer.run('list', [ list_data])
-    assert world.result == 0, "list failed: %d\n%s" % (exitcode, p.stderr.read())
+    (world.result, world.output, world.stderr ) = quixplorer.run('list', [ list_data ])
+    assert world.result == 0, "list failed: %d\n%s\n%s" % (world.result, world.output, world.stderr)
 
 @step(u'I find %(file)s in list' % { "file": Expressions.file } )
 def I_find_file_xx_in_list(step, filename):
